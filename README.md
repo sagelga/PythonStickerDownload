3ecc873607e95c82f8e2306a4f93dddcaa8b612f
Kunanon Srisuntiroj <son9912@gmail.com>
2018-04-21 01:18:52 +0700

    It can work now. lel

---
+++ README.md
@@ -0,0 +1,11 @@
+# PythonStickerDownloader
+This project is about automation of sticker downloader.
+
+Please do not use this repository to download the file and does not support the content creator.
+
+## How to contribute
+1. Fork
+2. Edit
+3. Create pull request
+4. Wait
+5. Celebrate!!!!
--- run.py
+++ run.py
@@ -1,5 +1,48 @@
-def main():
+# Installing the libraries
+import urllib
+import os, sys
+from PIL import Image
+from resizeimage import resizeimage
+
+url = "https://stickershop.line-scdn.net/stickershop/v1/sticker/36738878/ANDROID/sticker.png"
+file_name = ""
+file_extension = ".png"
+file_folder = "/result"
+count = 32
+
+def value_getter():
 	print("Please type in the URL")
 	url = input()
-	
+
+	print("How many stickers are in that pack?")
+	count = int(input())
+
+	return url, count
+
+def pic_downloader(url, file_name, file_extension, count):
+	for i in range(count):
+		# Editing a URL
+		url = url.split('/')
+		int_url = int(url[6]) + i
+		url[6] = str(int_url)
+		url = "/".join(url)
+		file_name = file_folder + "Pic" + str(i) + file_extension
+
+		print("Downloading " + url + " to " + file_name)
+		urllib.urlretrieve(url, file_name)
+
+        pic_resizer()
+
+def pic_resizer():
+    img = Image.open(file_folder + )  # image extension *.png,*.jpg
+    new_width = 200
+    new_height = 300
+    img = img.resize((new_width, new_height), Image.ANTIALIAS)
+    img.save('output image name.png')  # format may what u want ,*.png,*jpg,*.gif
+
+def main():
+	if url == "" and count == 0:
+		value_getter()
+	pic_downloader(url, file_name, file_extension, count)
+
 main()
