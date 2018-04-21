66ddea67c2c6a66d9602708f073ec7ce460bec29
Kunanon Srisuntiroj <son9912@gmail.com>
2018-04-21 10:17:24 +0700

    Fix miscalculation URL

--- run.py
+++ run.py
@@ -3,64 +3,62 @@ import urllib
 import os, sys
 
 # sudo apt-get install python-imaging
+import PIL
 from PIL import Image
-from resizeimage import resizeimage
 
-url = "https://stickershop.line-scdn.net/stickershop/v1/sticker/36738878/ANDROID/sticker.png"
-file_name = ""
+url = "https://stickershop.line-scdn.net/stickershop/v1/sticker/36146094/ANDROID/sticker.png"
+file_header = "Pic"
 file_extension = ".png"
-file_folder = "/result"
-count = 32
+file_folder = ""
+count = 60
 
 def value_getter():
-	print("Please type in the URL")
-	url = input()
+    print("Please type in the URL")
+    url = input()
 
-	print("How many stickers are in that pack?")
-	count = int(input())
+    print("How many stickers are in that pack?")
+    count = int(input())
 
-	return url, count
+    return url, count
 
-def pic_downloader(url, file_name, file_extension, count):
-	for i in range(count):
-		# Editing a URL
-		url = url.split('/')
-		int_url = int(url[6]) + i
-		url[6] = str(int_url)
-		url = "/".join(url)
-		file_name = file_folder + "Pic" + str(i) + file_extension
+def pic_downloader(url, file_extension, count):
+    for i in range(count):
+        # Editing a URL
+        url = url.split('/')
+        if (i == 0):
+            int_url = int(url[6])
+        else:
+            int_url = int(url[6]) + 1
+        url[6] = str(int_url)
+        url = "/".join(url)
+        file_name = file_folder + file_header + str(i) + file_extension
 
-		print("Downloading " + url + " to " + file_folder + file_name)
-		urllib.urlretrieve(url, file_name)
+        print("Downloading " + str(int_url) + " to " + file_folder + file_name)
+        urllib.urlretrieve(url, file_name)
 
-		file_location = file_folder + file_name
-        pic_resizer(i, file_location)
+        file_location = file_folder + file_name
+
+		#pic_resizer(i, file_location)
 
 def pic_resizer(i, file_location):
 
     img = Image.open(file_location)
-    img_size = img.size
-
     # Need to resize one of the following photo to the 512px
 
-    old_height = img_size[0]
-    old_width = img_size[1]
+    old_height = img.size[0]
+    old_width = img.size[1]
 
-    if old_height > old_width:
-        scale = (old_height - old_width)/old_height
-        new_height = 512
-        new_width = old_width * scale
-    else:
-        scale = (old_width - old_height)/old_width
-        new_width = 512
-        new_height = old_height * scale
+    print(old_height + " x " old_width)
 
-    img = img.resize((new_width, new_height), Image.ANTIALIAS)
-    img.save(file_location)
+    baseheight = 512
+
+    hpercent = (baseheight / float(img.size[1]))
+    wsize = int((float(img.size[0]) * float(hpercent)))
+
+    img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
+    img.save(/)
 
 def main():
-	if url == "" and count == 0:
-		value_getter()
-	pic_downloader(url, file_name, file_extension, count)
+    pic_downloader(url, file_extension, count)
 
 main()
